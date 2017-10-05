079836695e005ec143f58d392ed0a131167daa0f
fornesarturo <fornesarturo@gmail.com>
2017-10-05 18:50:34 -0500

    Master Server Initial Version
    Master Node server and skeleton of Slave Node server.

---
+++ master/index.js
@@ -0,0 +1,41 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const ioClient = require('socket.io-client');
+
+var socketArray = []
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected: ' + socket['id']);
+
+	socketArray.push(socket['id']);
+
+	// Send Message.
+	socket.emit('message', 'Hola bola');
+	// io.to(socketid).emit('message', 'Hola bola');
+	
+	console.log(socketArray);
+
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+		let socketIndex = socketArray.indexOf(socket['id']);
+		socketArray.splice(socketIndex);
+		console.log(socketArray);
+	});
+
+	socket.on('message', function(msg){
+		console.log("Received message from \'"+ socket['id'] +"\': " + msg);
+	});
+});
+
+io.on('message', function(msg){
+	console.log("Received message: " + msg);
+})
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
