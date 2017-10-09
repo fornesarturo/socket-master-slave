cd4352ea24d4ae2dc88f30ece98b0c7b3f3f426b
miguel-mzbi <miguel.mzbi@gmail.com>
2017-10-09 18:02:50 -0500

    Cambios

--- slave/index.js
+++ slave/index.js
@@ -1,30 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var ioClient = require('socket.io-client')("http://10.43.92.32:3000");
+var ioClient = require('socket.io-client')("http://10.43.53.181:3000");
 var msgArray = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-ioClient.on('message', function(msg){
-  console.log(msg);
-  io.to(serverID).emit('message', "ppepino");
-});
-
-// Not tested. Simple implementation to test git on atom
 ioClient.on('splitMsg', function(msg){
   msgArray.push(msg);
+  console.log(msg);
+  if(msgArray.length == 2) {
+    console.log(msgArray[0][0].index + " \t\t| " + msgArray[1][0].index)
+    console.log(msgArray[0][0].message + " \t| " + msgArray[1][0].message)
+  }
 });
 
-// Not tested. Simple implementation to test git on atom
 ioClient.on('sendMsg', function(msg){
   ioClient.emit('mergeMsg', msgArray.pop());
 });
 
-ioClient.emit('message', "hola soy gera");
-
-http.listen(3000, function(){
-
-});
+ioClient.emit('message', "Hola Cassandra. Soy Miguel. Ppepnio");
