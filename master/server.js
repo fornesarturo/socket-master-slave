b22051e8f6f2a3a2ca256738d0cd3b34208499bc
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2017-10-12 17:46:25 -0500

    Bugs fixed

--- master/server.js
+++ master/server.js
@@ -13,32 +13,53 @@ app.use(express.static('public'))
 
 
 io.on('connect', function(socket){
-	console.log('A user connected: ' + socket['id']);
-	socketArray.push(socket['id']);
-	socket.emit('connectedUsers', socket['id']);
+
+	socket.on('message', function(msg){
+		console.log('A user connected: ' + socket['id']);
+		console.log(msg);
+		socketArray.push(socket['id']);
+		console.log(socketArray.length+" length")
+		io.emit('connectedUsers', socketArray.length);
+
+	})
+
+	socket.on('sendSplittedMessage', function(messageArray){
+
+		var array = messageArray;
+		var j = 0;
+		for(var i = 0; i<socketArray.length; i++){
+			io.to(socketArray[i]).emit('deleteArray', "Delete all, please");
+			console.log("entre "+i)
+			sendMessage(array, j, socketArray[i]);
+			sendMessage(array, j+1, socketArray[i]);
+			j++;
+		}
+	});
+
+	socket.on('recoverMessage', function(msg){
+		for(var i = 0; i<socketArray.length; i++){
+			io.to(socketArray[i]).emit('sendMsg', msg)
+		}
+	});
+
+	socket.on('mergeMsg', function(msg){
+		if(msg!=undefined && msg!= null){
+			io.emit('mergeResults', msg);
+		}
+	})
+
 	socket.on('disconnect', function(){
-		console.log('User disconnected');
 		let socketIndex = socketArray.indexOf(socket['id']);
-		socketArray.splice(socketIndex);
-		socket.emit('disconnectedUser', socket['id']);
+		console.log(socketIndex)
+		if(socketIndex){
+			console.log('User disconnected DIFERENCIADOOOO');
+			let socketIndex = socketArray.indexOf(socket['id']);
+			socketArray.splice(socketIndex);
+			io.emit('disconnectedUser', socketArray.length);
+		}
 	});
 });
 
-io.on('sendSplittedMessage', function(messageArray){
-	var array = messageArray;
-	var j = 0;
-	for(var i = 0; i<socketArray.length; i++){
-		sendMessage(array, j, socketArray[i]);
-		sendMessage(array, j+1, socketArray[i]);
-		j++;
-	}
-});
-
-io.on('recoverMessage', function(msg){
-	for(var i = 0; i<socketArray.length; i++){
-		io.to(socketArray[i]).emit('sendMsg', msg)
-	}
-});
 
 function sendMessage(array, index, socketId){
 	var message = {message: array[index%3], index: index%3}
@@ -48,4 +69,5 @@ function sendMessage(array, index, socketId){
 
 http.listen(3006, function(){
 	console.log('listening on *:3006');
+	io.emit('refresh', "Location refresh");
 });
